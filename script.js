924dbbda074b5077b20f9d285ab96eedd958aee6
Alexandra <alexandrita@ukr.net>
2019-07-28 14:11:43 +0300

    Add

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.querySelector('tr').onclick = function(e) {
   var target = e.target;
-  var cnt = target.closest('td').querySelector('.text');
+  var count = target.closest('td').querySelector('.text');
   if (target.className === 'plus') {
-    cnt.innerText = parseInt(cnt.innerText, 10)+1;
+    count.innerText++;
   } else if (target.className === 'minus') {
-    cnt.innerText = parseInt(cnt.innerText, 10)-1;
-  } else return;
+    count.innerText--;
+  }
 };
